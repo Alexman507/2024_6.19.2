11342315e4588711d92e93f244a9e7eace71ff26
Alex_PC <nerocross@gmail.com>
2024-05-06 00:31:28 +0300

    correct views for hw

--- catalog/views.py
+++ catalog/views.py
@@ -3,18 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'You have new message from {name}({email}): {message}')
+    # if request.method == 'POST':
+    #     name = request.POST.get('name')
+    #     email = request.POST.get('email')
+    #     message = request.POST.get('message')
+    #     print(f'You have new message from {name}({email}): {message}')
     return render(request, 'main/home.html')
 
 
 def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        email = request.POST.get('email')
+        phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'You have new message from {name}({email}): {message}')
+        print(f'You have new message from {name}({phone}): {message}')
     return render(request, 'main/contacts.html')
